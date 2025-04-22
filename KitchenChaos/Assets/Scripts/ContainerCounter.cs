1f6e78525df3ba6ef9f0be5571589cda20357d1f
EVassiliev1983 <vassilievenator@gmail.com>
2025-04-22 13:18:14 +0300

    CountersWithFoodAdded

--- KitchenChaos/Assets/Scripts/ContainerCounter.cs
+++ KitchenChaos/Assets/Scripts/ContainerCounter.cs
@@ -12,9 +12,14 @@ public class ContainerCounter : BaseCounter
 
     public override void Interact(Player player)
     {
-        Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab);
-        kitchenObjectTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(player);
+        if(!player.HasKitchenObject())
+        {
+            //When player does not carry anything
+            Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab);
+            kitchenObjectTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(player);
+
+            OnPlayerGrabbedObject?.Invoke(this, EventArgs.Empty);
+        }
         
-        OnPlayerGrabbedObject?.Invoke(this, EventArgs.Empty);
     }
 }
