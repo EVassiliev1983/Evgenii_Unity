fc30e424c335893ea07f7fd92d6343871e16e586
EVassiliev1983 <vassilievenator@gmail.com>
2025-04-27 14:47:04 +0300

    Revert "CannotPutOnPlate"
    This reverts commit ee340f740f6e28db0d1cba408d1142a80f0db071.

--- KitchenChaos/Assets/Scripts/Counters/ClearCounter.cs
+++ KitchenChaos/Assets/Scripts/Counters/ClearCounter.cs
@@ -28,29 +28,8 @@ public class ClearCounter : BaseCounter
             //There is a KitchenObject here
             if (player.HasKitchenObject())
             {
-                //Player is carrying something
-                if (player.GetKitchenObject().TryGetPlate(out PlateKitchenObject plateKitchenObject))
-                {
-                    //Player is holding a plate
-                    if (plateKitchenObject.TryAddIngredient(GetKitchenObject().GetKitchenObjectSO()))
-                    {
-                        GetKitchenObject().DestroySelf();
-                    }
-                    else
-                    {
-                        //Player is not holding a plate but something else
-                        if (GetKitchenObject().TryGetPlate(out plateKitchenObject))
-                        {
-                            //There is a plate on the counter
-                            if (plateKitchenObject.TryAddIngredient(player.GetKitchenObject().GetKitchenObjectSO()))
-                            {
-                                player.GetKitchenObject().DestroySelf(); 
-                            }
-                        }
-                    }
-                }
+                //Player is carrying something 
             }
-
             else
             {
                 //Player is not carrying anything
